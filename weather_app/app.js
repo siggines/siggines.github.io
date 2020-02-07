f14bf8fc64982836a2bd60bc1e419380bf61901b
Jacob Siggins <siggins@protonmail.com>
2020-02-07 05:35:50 +0000

    Weather App

---
+++ weather_app/app.js
@@ -0,0 +1,41 @@
+window.addEventListener('load', ()=> {
+    let long;
+    let lat;
+    let temperatureDescription = document.querySelector(".temperature-description");
+    let temperatureDegree = document.querySelector(".temperature-degree");
+    let locationTimezone = document.querySelector(".location-timezone");
+    let fah;
+    let cel;
+    let temp;
+    let que;
+
+    if(navigator.geolocation){
+        navigator.geolocation.getCurrentPosition(position => {
+            long = position.coords.longitude;
+            lat = position.coords.latitude;
+
+            const proxy = "https://cors-anywhere.herokuapp.com/"
+            const api = `${proxy}https://api.darksky.net/forecast/e4be2535a1a7f99bdcdac04a9466214d/${lat},${long}`;
+            const api_windy = `${proxy}https://api.windy.com/point-forecast/h1BNNFesa7krzTbEeD2d56izmmfS5Y6E/${lat},${long}`;
+            
+            fetch(api)
+            .then(response =>{
+                return response.json();
+            })
+            .then(data =>{
+                console.log(data);
+                const {temperature, summary}= data.currently;
+                
+                fah = temperature;
+                cel = (fah-32)/1.8;
+                temp = Math.round(cel);
+                que = temp.toString() + ' celcius';
+                temperatureDegree.textContent = que;
+                temperatureDescription.textContent = summary;
+                locationTimezone.textContent = data.timezone;
+            })
+        });
+    }else{
+        h1.text = "Unable to retrieve location"
+    }
+});
