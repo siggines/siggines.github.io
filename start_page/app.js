d5e8a84f54d99dedb48133159394d9f783fee0cd
jacob <siggins@protonmail.com>
2020-05-12 07:41:33 +0100

    added css transitions, and js triggers

--- start_page/app.js
+++ start_page/app.js
@@ -12,3 +12,27 @@ function attach(){
 	window.location = together;
 	board.value="";
 }
+
+// "mouseout" would be applicable if I wanted the (div) element's 'children' included
+
+var div = document.querySelectorAll("div");
+
+var box = document.querySelector(".child h3");
+
+var TEST = document.getElementsByClassName("TEST");
+
+div[1].addEventListener("mouseleave",function(){
+	TEST[0].style.animation = "fade 2.3s linear";
+});
+div[2].addEventListener("mouseleave",function(){
+	TEST[1].style.animation = "fade 2.3s linear";
+});
+div[3].addEventListener("mouseleave",function(){
+	TEST[2].style.animation = "fade 2.3s linear";
+});
+div[4].addEventListener("mouseleave",function(){
+	TEST[3].style.animation = "fade 2.3s linear";
+});
+div[5].addEventListener("mouseleave",function(){
+	TEST[4].style.animation = "fade 2.3s linear";
+});
