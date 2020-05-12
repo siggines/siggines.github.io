6b6f5b80e87b1fd60b6cf1f0e5f91e2b3c84bc2f
jacob <siggins@protonmail.com>
2020-05-12 20:14:50 +0100

    Some last touches

--- start_page/clock.js
+++ start_page/clock.js
@@ -6,7 +6,7 @@ function startTime() {
     h = checkTime(h);
     m = checkTime(m);
     s = checkTime(s);
-    document.getElementById("clock").innerHTML = h+":"+m+":"+s;
+    document.getElementById("clock").innerHTML = h+":"+m; //+":"+s;
     var t = setTimeout(function(){startTime()},500);
 }
 
