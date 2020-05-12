c82299aea2502bf5f5bc75935849d08e52cf7873
jacob <siggins@protonmail.com>
2020-05-12 02:55:09 +0100

    Added click to copy function to homepage and some other changes

---
+++ app.js
@@ -0,0 +1,23 @@
+function COPY(specify){
+
+	var range = document.createRange();
+	var copied;
+
+	if( specify == 1 ){
+		range.selectNode(document.getElementById("mail1"));
+		copied = document.getElementById("copied1");
+		copied.classList.add("fader");
+	}
+
+	else if( specify == 2 ){
+		range.selectNode(document.getElementById("mail2"));
+		copied = document.getElementById("copied2");
+		copied.classList.add("fader");
+	}
+
+	window.getSelection().removeAllRanges(); // clear current selection
+	window.getSelection().addRange(range); // to select text
+	document.execCommand("copy");
+	window.getSelection().removeAllRanges(); // to deselect
+
+}
