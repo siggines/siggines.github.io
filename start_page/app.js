23ae27bf0e030dcc0e95d4a3ab059ef09e7e805a
jacob <siggins@protonmail.com>
2020-05-12 15:20:52 +0100

    Added reset to javascript animation triggers

--- start_page/app.js
+++ start_page/app.js
@@ -17,22 +17,44 @@ function attach(){
 
 var div = document.querySelectorAll("div");
 
-var box = document.querySelector(".child h3");
+//var box = document.querySelector(".child h3");
 
 var TEST = document.getElementsByClassName("TEST");
 
+var i; // Should replace manual index arguments with something to fetch whatever the index number is.
+
+function reset_animation(i) {
+	TEST[i].style.animation = "none";
+	TEST[i].style.animation = null;
+}
+
 div[1].addEventListener("mouseleave",function(){
 	TEST[0].style.animation = "fade 2.3s linear";
+	setTimeout(function() {
+		reset_animation(0)
+    	}, 2300 );
 });
 div[2].addEventListener("mouseleave",function(){
 	TEST[1].style.animation = "fade 2.3s linear";
+	setTimeout(function() {
+		reset_animation(1)
+    	}, 2300 );
 });
 div[3].addEventListener("mouseleave",function(){
 	TEST[2].style.animation = "fade 2.3s linear";
+	setTimeout(function() {
+		reset_animation(2)
+    	}, 2300 );
 });
 div[4].addEventListener("mouseleave",function(){
 	TEST[3].style.animation = "fade 2.3s linear";
+	setTimeout(function() {
+		reset_animation(3)
+    	}, 2300 );
 });
 div[5].addEventListener("mouseleave",function(){
 	TEST[4].style.animation = "fade 2.3s linear";
+	setTimeout(function() {
+		reset_animation(4)
+    	}, 2300 );
 });
