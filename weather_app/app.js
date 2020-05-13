f830dea44b2a08caeb50df42a45f9c96e53938af
jacob <siggins@protonmail.com>
2020-05-13 21:43:50 +0100

    update

--- weather_app/app.js
+++ weather_app/app.js
@@ -1,4 +1,33 @@
-var getIP = 'https://siggines.github.io/weather_app/proxy/api.htm';
+//var IP = "";
+//var LAT = "";
+//var LON = "";
+
+/*$.getJSON('https://api.ipify.org?format=json', function(data){
+	IP = data.ip;
+});*/
+
+//var geocoder = new google.maps.Geocoder();
+//geocoder.geocode( { 'address': address}, function(results, status){
+//	var location = results[0].geometry.location;
+//	LAT = location.lat()
+//	LON = location.lng();
+//});
+
+//var getIP = 'http://ip-api.com/json/';
+//var getIP = 'http://www.geoplugin.net/json.gp?ip='+IP;
+/*function test(){ locate() }
+function locate(){
+	console.log("FUCK");
+	if(navigator.geolocation){
+		navigator.geolocation.getCurrentPosition((position)=>{
+			const lat  = position.coords.latitude;
+			const lon = position.coords.longitude;
+			console.log(lat,lon);
+		});
+	}
+	console.log(lat,lon);
+}*/
+
 var openWeatherMap = 'https://api.openweathermap.org/data/2.5/weather'
 
 var LOC = document.getElementById("loc");
@@ -7,13 +36,26 @@ var temp = document.getElementById("temp");
 var wind = document.getElementById("wind");
 var desc = document.getElementById("desc");
 
-$.getJSON(getIP).done(function(location){
+var LAT = "";
+var LON = "";
+
+navigator.geolocation.getCurrentPosition(getLatLon);
+
+function getLatLon(position) {
+	var latitude = position.coords.latitude;
+	var longitude = position.coords.longitude;
+	console.log("Latitude is "+latitude);
+	console.log("Longitude is "+longitude);
+	LAT = latitude;
+	LON = longitude;
+
 	$.getJSON(openWeatherMap, {
-		lat: location.lat,
-		lon: location.lon,
+		lat: LAT,
+		lon: LON,
 		units: 'metric',
 		APPID: '2165aab16a1fba518e53a0da29ba38d5'
-	}).done(function(weather) {
+	}
+	).done(function(weather){
 		console.log(weather);
 
 		LOC.textContent = weather.name;
@@ -23,4 +65,4 @@ $.getJSON(getIP).done(function(location){
 		desc.textContent = weather.main.humidity+" %";
 
 	})
-})
+}
