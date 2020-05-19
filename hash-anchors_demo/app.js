5d667e750dcd1353738d1e3b4be8fb57cb9db733
jacob <siggins@protonmail.com>
2020-05-19 01:17:20 +0100

    birds!

--- hash-anchors_demo/app.js
+++ hash-anchors_demo/app.js
@@ -1,47 +1,72 @@
 /////////////////////////////////////////////////////
-//	Changes page on hash change + loads images
+//	Loads relevant content on hash change
 ////////////////////////////////////////////////////
 
 var image = document.getElementsByClassName("image");
 
 var readOut = document.getElementById("text");
+/* readOut.textContent = ""; */
 
-var doc = "";
+var list_readOut = document.getElementById("species");
+list_readOut.textContent = "";
+
+var x,n;
+var count = 0;
+
+/* TO-DO:
+
+	· Make 'read_this()' return value in 'read()' instead of defining 'x'
+
+*/
+function read_this(FILE){
+
+	var rawFile = new XMLHttpRequest();
+	rawFile.open("GET", FILE, false);
+	rawFile.onreadystatechange = function ()
+	{
+		if(rawFile.readyState === 4)
+		{
+			if(rawFile.status === 200 || rawFile.status == 0)
+			{
+				x = rawFile.responseText;
+			}
+		}
+	}
+	rawFile.send(null);
+
+}
 
 function read(){
 	if(location.hash){
-		var file = "./doc/"+(location.hash);
-		var img = "./pix/"+(location.hash);
-		file = file.replace("#","");
-		img = img.replace("#","");
-		image[0].src = img + ".png" ;
-		/*image[1].src = img + "2" + ".png" ;
-		image[2].src = img + "3" + ".png" ;
-		image[3].src = img + "4" + ".png" ;
-		image[4].src = img + "5" + ".png" ;
-		image[5].src = img + "6" + ".png" ;*/
-		var rawFile = new XMLHttpRequest();
-		rawFile.open("GET", file, false);
-		rawFile.onreadystatechange = function ()
-		{
-			if(rawFile.readyState === 4)
-			{
-				if(rawFile.status === 200 || rawFile.status == 0)
-				{
-					console.log(rawFile.responseText);
-					var allText = rawFile.responseText;
-					console.log(allText);
-					doc = allText;
-				}
-			}
-		}
-		rawFile.send(null);
+
+		var ref=(location.hash);
+		ref = ref.replace("#","");
+
+		var file = "./doc/"+ref;
+
+		var img = "./pix/"+ref;
+		var imgNum = img+"_count";
+		var imgArray = [ img,img+"2",img+"3",img+"4",img+"5",img+"6" ];
+
+		var list = "./doc/"+ref+"_list";
+
+		read_this(file);
+		readOut.textContent = x;
+
+		read_this(imgNum);
+		count = Number(x);
+
+		for( n=0; n<count; ++n )
+			image[n].src = imgArray[n] + ".png" ;
+
+		read_this(list);
+		list_readOut.textContent = x;
+
 	}
-	readOut.textContent = doc;
 }
 
 /////////////////////////////////////////////////////
-//	On image hover, and on image click
+//	On image click
 ////////////////////////////////////////////////////
 
 var cover = document.getElementsByClassName("cover");
@@ -56,3 +81,18 @@ function exit(){
 	image[0].classList.remove("open");
 	cover[0].classList.remove("on");
 }
+
+////////////////////////////////////////////////////
+//	Customised Selection Menu		[Unfinished]
+/////////////////////////////////////////////////////
+
+var i;
+
+var wrapper = document.getElementsByClassName("selections");
+
+var actual = document.getElementsByClassName("list");
+
+function custom(){
+	/*actual[0].classList.add("custom");*/
+}
+
