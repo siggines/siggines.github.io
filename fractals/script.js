c815835590098bc003f851d97d4460e6fb529b4f
jacob <siggins@protonmail.com>
2020-06-01 22:22:28 +0100

    ²

--- fractals/script.js
+++ fractals/script.js
@@ -4,34 +4,34 @@ canvas.height = "1000";
 document.body.appendChild(canvas);
 var ctx = canvas.getContext("2d");
 
-var mag = 600;
-var Xpan = 0; var Ypan = 0;
-
 function check(x,y){ //returns true or false
 	var X = x; var Y = y;
 
 	var Xsqr = X*X; var Ysqr = Y*Y; // Squared
-	var XX = X*X; var YY = Y*Y;	// Doubled
+	var XX = X*2;	// Doubled
 
 	for( var i = 0; i < 10; ++i ){
-		X = Xsqr - Ysqr + x;
-		Y = XX*YY;
+		var Xn = X*X - Y*Y + x;
+		var Yn = 2* X * Y + y;
+		X=Xn; Y=Yn;
 	}
 
-	if( X+Y < 5 )
+	if( X*Y < 5 )
 		return true;
-	else
-		return false;
+
+	return false;
 }
 
 function render(){
+	var mag = 100;
+	var Xpan = 0; var Ypan = 0;
 	for( var x = 0; x < canvas.width; ++x ){
 		for( var y = 0; y < canvas.height; ++y ){
 			var Xready = x/mag-Xpan;
 			var Yready = y/mag-Ypan;
 			var CHECK = check(Xready,Yready);
 			if(CHECK){
-				ctx.fillRect(y,x, 1,1);
+				ctx.fillRect(x,y, 1,1);
 			}
 		}
 	}
