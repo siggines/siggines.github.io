0812f8e31d8ca8c4c33527336fce6ff02d5c75ac
jacob <siggins@protonmail.com>
2020-06-06 22:30:33 +0100

    Fresh

--- start_page/app.js
+++ start_page/app.js
@@ -1,29 +1,16 @@
-var chan = document.getElementById("chan");
-var board = "";
-
-function enter(input){
-	if(event.key === 'Enter'){
-		board = input.value;
-	}
-}
-
-function attach(){
-	var together = "https://boards.4channel.org/"+board+"/catalog";
-	window.location = together;
-	board.value="";
-}
-
 ///////////////////////////////////////////////////////////////////////////////////////
 // Box title fadeback on mouse leave animations, canceled when mosue moves back in
 // "mouseout" would be applicable if I wanted the (div) element's 'children' included
 // I use reset_animation to allow animation to happen multiple times and also
 // 	to 'cancel' the animation if you were to move mouse out and back in of box
-// I have also had to set the title display to none when links are visible otherwise
-//	the element takes up space and makes the top link(s) partially unclickable
+//
+// For Some reason eventListeners in commented out for loop will not work so went through manually
+//	advice appreciated lol
 ///////////////////////////////////////////////////////////////////////////////////////
 
 var div = document.querySelectorAll("div");
 var title = document.getElementsByClassName("title");
+//var len = div.length;
 
 function reset_animation(i) {
 	title[i].style.animation = "none";
@@ -38,37 +25,79 @@ function fade_back(i){
     	}, 3000 );
 }
 
-div[1].addEventListener("mouseleave",function(){
+div[2].addEventListener("mouseleave",function(){
 	fade_back(0);
 });
-div[1].addEventListener("mouseover",function(){
+div[2].addEventListener("mouseover",function(){
 	reset_animation(0);
 });
 
-div[2].addEventListener("mouseleave",function(){
+div[3].addEventListener("mouseleave",function(){
 	fade_back(1);
 });
-div[2].addEventListener("mouseover",function(){
+div[3].addEventListener("mouseover",function(){
 	reset_animation(1);
 });
 
-div[3].addEventListener("mouseleave",function(){
+div[4].addEventListener("mouseleave",function(){
 	fade_back(2);
 });
-div[3].addEventListener("mouseover",function(){
+div[4].addEventListener("mouseover",function(){
 	reset_animation(2);
 });
 
-div[4].addEventListener("mouseleave",function(){
+div[5].addEventListener("mouseleave",function(){
 	fade_back(3);
 });
-div[4].addEventListener("mouseover",function(){
+div[5].addEventListener("mouseover",function(){
 	reset_animation(3);
 });
 
-div[5].addEventListener("mouseleave",function(){
+div[6].addEventListener("mouseleave",function(){
 	fade_back(4);
 });
-div[5].addEventListener("mouseover",function(){
+div[6].addEventListener("mouseover",function(){
 	reset_animation(4);
 });
+
+div[7].addEventListener("mouseleave",function(){
+	fade_back(5);
+});
+div[7].addEventListener("mouseover",function(){
+	reset_animation(5);
+});
+
+div[8].addEventListener("mouseleave",function(){
+	fade_back(6);
+});
+div[8].addEventListener("mouseover",function(){
+	reset_animation(6);
+});
+
+/*for( var x = 1; x <= len; ){
+	function reset_animation(i) {
+		title[i].style.animation = "none";
+		title[i].style.animation = null;
+	}
+
+	function fade_back(i){
+		title[i].style.display = "block";
+		title[i].style.animation = "fade 3s linear";
+		setTimeout(function() {
+			reset_animation(i);
+		}, 3000 );
+	}
+	var i = x-2;
+	var div = document.querySelectorAll("div");
+	var title = document.getElementsByClassName("title");
+
+	div[x].addEventListener("mouseleave",function(){
+		fade_back(i);
+	});
+	div[x].addEventListener("mouseover",function(){
+		reset_animation(i);
+	});
+
+	console.log("?");
+	++x
+}*/
