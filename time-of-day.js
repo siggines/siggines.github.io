32a3b338c241f0aa9189b01f7bd5771dbc0deb12
jacob <jacob@fedora.Home>
2021-04-27 18:36:08 +0100

    fix

--- time-of-day.js
+++ time-of-day.js
@@ -3,6 +3,6 @@ var hour=date.getHours();
 
 var good = "Good "
 var insert = document.getElementById("time-of-day");
-if( hour <= 12 ){ insert.innerHTML = good+"morning..." }
-else if( hour <= 18 ){ insert.innerHTML = good+"afternoon..." }
-else if( hour <= 24 ){ insert.innerHTML = good+"evening..." }
+if( hour < 12 ){ insert.innerHTML = good+"morning..." }
+else if( hour < 18 ){ insert.innerHTML = good+"afternoon..." }
+else if( hour < 24 ){ insert.innerHTML = good+"evening..." }
